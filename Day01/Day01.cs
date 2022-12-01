05c6370c4bcc36c3fd200cb8c153ff315b5ad4ee
Luc Dorval <ldorval@gmail.com>
2022-12-01 12:29:44 -0500

    Day 1 !

---
+++ Day01/Day01.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2022.Day01;
+
+public class Day01
+{
+    public static int Solve(string input, int elfCount)
+    {
+        return input.Split("\r\n\r\n")
+            .Select(x => x.Split("\r\n"))
+            .Select(x => x.Select(y => Convert.ToInt32(y)).Sum())
+            .OrderByDescending(x => x)
+            .Take(elfCount)
+            .Sum();
+    }
+}
---
+++ Day01/Day01Test.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2022.Day01;
+
+public class Day01Test
+{
+    private const string example = "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000";
+    
+    [Test]
+    public void ExamplePart1()
+    {
+        Day01.Solve(example, 1).Should().Be(24000);
+    }
+
+    [Test]
+    public void SolutionPart1()
+    {
+        Console.WriteLine(Day01.Solve("Day01.txt".ReadAll(), 1));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        Day01.Solve(example, 3).Should().Be(45000);
+    }
+    
+    [Test]
+    public void SolutionPart2()
+    {
+        Console.WriteLine(Day01.Solve("Day01.txt".ReadAll(), 3));
+    }
+}
