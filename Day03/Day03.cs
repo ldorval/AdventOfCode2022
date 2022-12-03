adfb0761aa65693c5c0e1ab8caf2fb361bfc2195
Luc Dorval <ldorval@gmail.com>
2022-12-03 10:11:10 -0500

    Day 3 !

---
+++ Day03/Day03.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode2022.Day03;
+
+public class Day03
+{
+    public static int Solve(List<string> ruckSacks)
+    {
+        return ruckSacks
+            .Select(x => new List<List<char>> { x.Take(x.Length / 2).ToList(), x.TakeLast(x.Length / 2).ToList() })
+            .Select(x => x.First().Intersect(x.Last()).Single())
+            .Select(GetPriority)
+            .Sum();
+    }
+
+    public static int SolvePart2(List<string> ruckSacks)
+    {
+        var groups = new List<List<string>>();
+        
+        for (var i = 0; i < ruckSacks.Count; i += 3)
+        {
+            groups.Add(ruckSacks.Skip(i).Take(3).ToList());
+        }
+
+        return groups
+            .Select(x => x.Select(y => y.ToCharArray()))
+            .Select(x => x.Aggregate((a, b) => a.Intersect(b).ToArray()).Single())
+            .Select(GetPriority)
+            .Sum();
+    }
+
+    private static int GetPriority(char x)
+    {
+        return char.IsUpper(x) ? x - 38 : x - 96;
+    }
+}
---
+++ Day03/Day03Test.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2022.Day03;
+
+public class Day03Test
+{
+    [Test]
+    public void ExamplePart1()
+    {
+        Day03.Solve("Day03Example.txt".ReadAll().LinesToString()).Should().Be(157);
+    }
+
+    [Test]
+    public void SolutionPart1()
+    {
+        Console.Write(Day03.Solve("Day03.txt".ReadAll().LinesToString()));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        Day03.SolvePart2("Day03Example.txt".ReadAll().LinesToString()).Should().Be(70);
+    }
+    
+    [Test]
+    public void SolutionPart2()
+    {
+        Console.Write(Day03.SolvePart2("Day03.txt".ReadAll().LinesToString()));
+    }
+}
