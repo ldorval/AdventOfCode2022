55f769cc12bc1dce539c657ca027888f54b84384
Luc Dorval <ldorval@gmail.com>
2022-12-04 17:28:04 -0500

    Day 4

---
+++ Day04/Day04.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode2022.Day04;
+
+public class Day04
+{
+    public static int Solve(List<string> input)
+    {
+        return input
+            .Select(x => x.Split(","))
+            .Count(x => OneAssignmentContainsTheOther(GetRange(x[0]), GetRange(x[1])));
+    }
+
+    public static int SolvePart2(List<string> input)
+    {
+        return input
+            .Select(x => x.Split(","))
+            .Count(x => AssignmentsOverlap(GetRange(x[0]), GetRange(x[1])));
+    }
+
+    public static bool OneAssignmentContainsTheOther((int Start, int End) elf1, (int Start, int End) elf2)
+    {
+        return (elf1.Start <= elf2.Start && elf1.End >= elf2.End) ||
+               (elf2.Start <= elf1.Start && elf2.End >= elf1.End);
+    }
+
+    public static bool AssignmentsOverlap((int Start, int End) elf1, (int Start, int End) elf2)
+    {
+        return (elf1.End >= elf2.Start && elf1.Start <= elf2.End) ||
+               (elf2.End >= elf1.Start && elf2.Start <= elf1.End);
+    }
+
+    public static (int Start, int End) GetRange(string assignment)
+    {
+        var parts = assignment.Split("-");
+        return (Convert.ToInt32(parts[0]), Convert.ToInt32(parts[1]));
+    }
+}
---
+++ Day04/Day04Test.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2022.Day04;
+
+public class Day04Test
+{
+    [Test]
+    public void GetsElfAssignmentRange()
+    {
+        Day04.GetRange("1-4").Should().Be((1, 4));
+        Day04.GetRange("43-54").Should().Be((43, 54));
+    }
+
+    [Test]
+    public void ValidatesIfOneAssignmentContainsTheOther()
+    {
+        Day04.OneAssignmentContainsTheOther((3, 7), (2, 8)).Should().BeTrue();
+        Day04.OneAssignmentContainsTheOther((2, 8), (3, 7)).Should().BeTrue();
+        
+        Day04.OneAssignmentContainsTheOther((1, 5), (2, 8)).Should().BeFalse();
+        Day04.OneAssignmentContainsTheOther((2, 8), (1, 5)).Should().BeFalse();
+    }
+    
+    [Test]
+    public void ExamplePart1()
+    {
+        Day04.Solve("Day04Example.txt".ReadAll().LinesToString()).Should().Be(2);
+    }
+
+    [Test]
+    public void SolutionPart1()
+    {
+        Console.Write(Day04.Solve("Day04.txt".ReadAll().LinesToString()));
+    }
+
+    [Test]
+    public void ValidatesIfAssignmentsOverlap()
+    {
+        Day04.AssignmentsOverlap((5, 7), (7, 9)).Should().BeTrue();
+        Day04.AssignmentsOverlap((7, 9), (5, 7)).Should().BeTrue();
+        Day04.AssignmentsOverlap((2, 8), (3, 7)).Should().BeTrue();
+        
+        Day04.AssignmentsOverlap((2, 3), (4, 5)).Should().BeFalse();
+        Day04.AssignmentsOverlap((4, 5), (2, 3)).Should().BeFalse();
+    }
+    
+    [Test]
+    public void ExamplePart2()
+    {
+        Day04.SolvePart2("Day04Example.txt".ReadAll().LinesToString()).Should().Be(4);
+    }
+
+    [Test]
+    public void SolutionPart2()
+    {
+        Console.Write(Day04.SolvePart2("Day04.txt".ReadAll().LinesToString()));
+    }
+}
