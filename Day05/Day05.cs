33ea13978dbcc313bf5b71b96f1f406281d30ada
Luc Dorval <ldorval@gmail.com>
2022-12-05 12:24:09 -0500

    Day 5

---
+++ Day05/Day05.cs
@@ -0,0 +1,57 @@
+﻿namespace AdventOfCode2022.Day05;
+
+public class Day05
+{
+    public static string Solve(Dictionary<int,Stack<char>> stacks, List<string> inputLines)
+    {
+        GetInstructions(inputLines).ForEach(x =>
+        {
+            for (var i = 0; i < x.count; i++)
+            {
+                var crate = stacks[x.from].Pop();
+                stacks[x.to].Push(crate);
+            }
+        });
+        
+        return GetTopOfStacks(stacks);
+    }
+
+    public static string SolvePart2(Dictionary<int, Stack<char>> stacks, List<string> inputLines)
+    {
+        GetInstructions(inputLines).ForEach(x =>
+        {
+            var poppedCrates = new List<char>();
+            for (var i = 0; i < x.count; i++)
+            {
+                poppedCrates.Add(stacks[x.from].Pop());
+            }
+
+            poppedCrates.Reverse();
+            poppedCrates.ForEach(y => stacks[x.to].Push(y));
+        });
+        return GetTopOfStacks(stacks);
+    }
+
+    private static string GetTopOfStacks(Dictionary<int, Stack<char>> stacks)
+    {
+        var output = "";
+        
+        for (var i = 1; i <= stacks.Count; i++)
+        {
+            output += stacks[i].Pop();
+        }
+
+        return output;
+    }
+
+    public static List<(int count, int from, int to)> GetInstructions(List<string> inputLines)
+    {
+        return inputLines.Select(x =>
+        {
+            var count = Convert.ToInt32(x.Substring(4, x.IndexOf("from") - 4));
+            var from = Convert.ToInt32(x.Substring(x.IndexOf("from") + 4, x.IndexOf("to") - x.IndexOf("from") - 4));
+            var to = Convert.ToInt32(x.Substring(x.IndexOf("to") + 2, x.Length - x.IndexOf("to") - 2));
+            return (count, from, to);
+        }).ToList();
+    }
+}
---
+++ Day05/Day05Test.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2022.Day05;
+
+public class Day05Test
+{
+    private Dictionary<int, Stack<char>> exampleStacks = new()
+    {
+        { 1, new Stack<char>(new List<char> { 'Z', 'N' }) },
+        { 2, new Stack<char>(new List<char> { 'M', 'C', 'D' }) },
+        { 3, new Stack<char>(new List<char> { 'P' }) }
+    };
+    
+    private Dictionary<int, Stack<char>> stacks = new()
+    {
+        { 1, new Stack<char>(new List<char> { 'Q', 'W', 'P', 'S', 'Z', 'R', 'H', 'D' }) },
+        { 2, new Stack<char>(new List<char> { 'V', 'B', 'R', 'W', 'Q', 'H', 'F' }) },
+        { 3, new Stack<char>(new List<char> { 'C', 'V', 'S', 'H' }) },
+        { 4, new Stack<char>(new List<char> { 'H', 'F', 'G' }) },
+        { 5, new Stack<char>(new List<char> { 'P', 'G', 'J', 'B', 'Z' }) },
+        { 6, new Stack<char>(new List<char> { 'Q', 'T', 'J', 'H', 'W', 'F', 'L' }) },
+        { 7, new Stack<char>(new List<char> { 'Z', 'T', 'W', 'D', 'L', 'V', 'J', 'N' }) },
+        { 8, new Stack<char>(new List<char> { 'D', 'T', 'Z', 'C', 'J', 'G', 'H', 'F' }) },
+        { 9, new Stack<char>(new List<char> { 'W', 'P', 'V', 'M', 'B', 'H' }) },
+    };
+    
+    [Test]
+    public void ExamplePart1()
+    {
+        Day05.Solve(exampleStacks, "Day05Example.txt".ReadAll().LinesToString()).Should().Be("CMZ");
+    }
+
+    [Test]
+    public void ParsesInstructions()
+    {
+        Day05.GetInstructions(new List<string> { "move 3 from 1 to 3", "move 2 from 2 to 1" }).Should().BeEquivalentTo(new List<object> {(3, 1, 3), (2, 2, 1)});
+    }
+
+    [Test]
+    public void SolutionPart1()
+    {
+        Console.Write(Day05.Solve(stacks, "Day05.txt".ReadAll().LinesToString()));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        Day05.SolvePart2(exampleStacks, "Day05Example.txt".ReadAll().LinesToString()).Should().Be("MCD");
+    }
+    
+    [Test]
+    public void SolutionPart2()
+    {
+        Console.Write(Day05.SolvePart2(stacks, "Day05.txt".ReadAll().LinesToString()));
+    }
+}
